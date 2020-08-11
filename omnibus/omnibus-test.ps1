9386d44fef934de4341865b539f41e1de237ebd7
Lamont Granquist <lamont@scriptkiddie.org>
2020-08-10 19:28:33 -0700

    disable choco tests again
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- omnibus/omnibus-test.ps1
+++ omnibus/omnibus-test.ps1
@@ -94,5 +94,5 @@ winrm quickconfig -quiet
 bundle
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
-bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f progress
+bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f progress --tag ~choco_installed
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
